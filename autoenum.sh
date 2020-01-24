4e9f1f5dfaba1e5fa26e6679ef46bdf5d4d2c6e2
root <root@localhost.localdomain>
2020-01-24 12:20:47 -0500

    autoenum.sh

--- autoenum.sh
+++ autoenum.sh
@@ -10,16 +10,14 @@ nmap_reg="nmap -p- -T4 -Pn -v $IP"
 # if aggresive
 if [[ ! -d "autoenum" ]];then mkdir autoenum; fi
 if [[ ! -d "autoenum/aggr_scan/raw" ]];then mkdir -p autoenum/aggr_scan/raw; fi
-if [[ ! -d "autoenum/aggr_scan/ports_and_services" ]];then  mkdir autoenum/aggr_scan/ports_and_services; fi
-if [[ ! -d "autoenum/aggr_scan/exploits" ]];then mkdir autoenum/exploits; fi
+if [[ ! -d "autoenum/aggr_scan/ports_and_services" ]];then  mkdir -p autoenum/aggr_scan/ports_and_services; fi
+if [[ ! -d "autoenum/aggr_scan/exploits" ]];then mkdir -p autoenum/aggr_scan/exploits; fi
 $nmap_aggr | tee -a autoenum/aggr_scan/raw/first_pass
 cat autoenum/aggr_scan/raw/first_pass | grep -i "discovered" | tee -a autoenum/aggr_scan/ports_and_services/ports_discovered
 cat autoenum/aggr_scan/raw/first_pass | grep "open" | awk -F 'Discovered' '{print $1}' | sed '/^$/d' | tee -a autoenum/aggr_scan/ports_and_services/services_running
 cat autoenum/aggr_scan/raw/first_pass | grep 'OS' | sed '1d' | sed '$d' | cut -d '|' -f 1 | sed '/^$/d' | tee -a autoenum/aggr_scan/ports_and_services/OS_detection
-cat autoenum/aggr_scan/raw/first_pass | grep "script results" | tee autoenum/aggr_scan/ports_and_services/script_output; cat autoenum/aggr_scan/raw | grep "|" | sed '$d' | tee -a autoenum/aggr_scan/ports_and_services/script_output
+cat autoenum/aggr_scan/raw/first_pass | grep "script results" | tee autoenum/aggr_scan/ports_and_services/script_output; cat autoenum/aggr_scan/raw/first_pass | grep "|" | sed '$d' | tee -a autoenum/aggr_scan/ports_and_services/script_output
 
-#first things first, update searchsploit
-#searchsploit -u
 # run nmap xml output thru searchsploit as a 'first sweep' and then run services names
 $nmap_aggr -oX autoenum/aggr_scan/raw/nmap_out.xml
 searchsploit -v --nmap -w autoenum/aggr_scan/raw/nmap_out.xml | tee -a autoenum/aggr_scan/exploits/searchsploit_nmap
@@ -29,7 +27,7 @@ searchsploit -v --nmap -w autoenum/aggr_scan/raw/nmap_out.xml | tee -a autoenum/
 #$nmap_reg $IP | tee -a reg_scan
 #cat reg_scan | grep -i "discovered" | tee -a ports_discovered
 
-###########################################################################################################################################################################
+#############################################################################################################################
 #for service in $(cat services); do searchsploit $service | tee -a searchsploit_$service # create method to remove files if no exploits are found
 
 
