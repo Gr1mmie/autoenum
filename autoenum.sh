f75224676b95c38aa5f4a985e07606f19a228e6e
root <root@localhost.localdomain>
2020-01-24 18:51:23 -0500

    autoenum.sh

--- autoenum.sh
+++ autoenum.sh
@@ -9,6 +9,7 @@ nmap_reg="nmap -p- -T4 -Pn -v $IP"
 
 # if aggresive
 if [[ ! -d "autoenum" ]];then mkdir autoenum; fi
+if [[ ! -d "autoenum/loot" ]];then mkdir autoenum/loot; fi
 if [[ ! -d "autoenum/aggr_scan/raw" ]];then mkdir -p autoenum/aggr_scan/raw; fi
 if [[ ! -d "autoenum/aggr_scan/ports_and_services" ]];then  mkdir -p autoenum/aggr_scan/ports_and_services; fi
 if [[ ! -d "autoenum/aggr_scan/exploits" ]];then mkdir -p autoenum/aggr_scan/exploits; fi
@@ -22,8 +23,16 @@ cat autoenum/aggr_scan/raw/first_pass | grep "script results" > autoenum/aggr_sc
 $nmap_aggr -oX autoenum/aggr_scan/raw/nmap_out.xml
 searchsploit -v --nmap -w autoenum/aggr_scan/raw/nmap_out.xml | tee -a autoenum/aggr_scan/exploits/searchsploit_nmap
 
-# if website, run nikto and bruteforce dirs using dirsearch to look for specific dirs or jsut dirbuster and output everything returned
-# cat autorecon/aggr_scan/ports_and_services/services_running | grep "http" | tee -a http_found;if [ -s 'http_found' ];then run nikto/dirb etc. fi; rm http_found
+# if website, run nikto and bruteforce dirs using dirsearch to look for specific dirs or just dirbuster and output everything returned
+cat autorecon/aggr_scan/ports_and_services/services_running | grep "http" | tee -a http_found
+if [ -s 'http_found' ]
+then
+	nikto -h $IP | tee -a autorecon/loot/nikto_output
+	dirb http://$IP | autorecon/loot/dirs
+	rm http_found
+else
+	rm http_found
+fi
 
 #############################################################################################################################
 #for service in $(cat services); do searchsploit $service | tee -a searchsploit_$service # create method to remove files if no exploits are found
