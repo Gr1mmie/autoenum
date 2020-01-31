66ae1e37a3619a5da79f113d6b0f27c209bf020e
root <root@localhost.localdomain>
2020-01-31 12:05:48 -0500

    autoenum.sh

--- autoenum.sh
+++ autoenum.sh
@@ -3,7 +3,7 @@
 IP=$1
 
 nmap_aggr="nmap -A -T4 -p- -Pn -v $IP"
-nmap_reg="nmap -p- -T4 -Pn -v $IP"
+#nmap_reg="nmap -p- -T4 -Pn -v $IP"
 
 # add conditional to choose which scan type, set aggresive to default.
 
@@ -41,7 +41,18 @@ searchsploit -v --nmap -w autoenum/aggr_scan/raw/nmap_out.xml | tee -a autoenum/
 # searchsploit smart search. need to come up with a generic way to pull entire service names, also want to make it so that if nothing is found with full string, remove trailing word and rerun
 cat autoenum/aggr_scan/ports_and_services/services_running | awk '{print($4,$5,$6,$7,$8,$9)}' | sort -u | awk 'NF' | tee autoenum/loot/services
 cat autoenum/loot/services | awk '{$1=$1};1' | tr ' ' '-' | tee autoenum/loot/services
-for service in $(cat autoenum/loot/services);do svc=$(echo $service | tr '-' ' ');echo "checking $svc...";sleep 3; searchsploit $svc | tee -a autoenum/loot/exploits/searchsploit_$svc;done
+for service in $(cat autoenum/loot/services);do
+	svc=$(echo $service | tr '-' ' '| tr '/' ' ')
+	echo "checking $svc..."
+	searchsploit "$svc" | tee "autoenum/loot/exploits/searchsploit_$svc"
+	if grep -q "Exploits: No Result" "autoenum/loot/exploits/searchsploit_$svc";then rm "autoenum/loot/exploits/searchsploit_$svc";fi
+#		if [[ ! "$(echo $svc wc -w)" -gt "3" ]];then rm autoenum/loot/exploits/searchsploit_$svc;fi
+#		while [[ "$(echo $svc | wc -w)" -gt 3 ]]; do
+#			svc=$(echo $svc | sed 's/\w*$//')
+#			searchsploit $svc | tee autoenum/loot/exploits/searchsploit_$svc
+#			if grep -q "Exploits: No Results" "autoenum/loot/exploits/searchsploit_$svc"; then rm autoenum/loot/exploits/searchsploit_$svc;fi
+#	fi
+done
 
 find autoenum/ -type d -empty -delete
 
@@ -52,16 +63,20 @@ if [ -s 'autoenum/aggr_scan/raw/http_found' ]
 		cat autoenum/aggr_scan/raw/http_found | cut -d '/' -f 1 >> autoenum/loot/http/ports
 		if [ -s 'autoenum/loot/http/ports' ]
 			then
+				mkdir -p autoenum/loot/http/dir
 				for port in $(cat autoenum/loot/http/ports)
 					do
-						nikto -h $IP:$port >> autoenum/loot/http/nikto_$port
-						dirb http://$IP:$port >> autoenum/loot/http/dirs_$port
-						rm autoenum/loot/http/ports
+						echo "running nikto on port $port"
+						nikto -h $IP:$port >> autoenum/loot/http/nikto_$port &
+						echo "bruteforcing dirs on $IP:$port"
+						gobuster dir -re -t 25 -w /usr/share/wordlists/dirb/common.txt -o autoenum/loot/http/dirs/raw &
+						for dir in $(cat autoenum/loot/http/dir); do gobuster dir -re -t 25 -w /usr/share/wordlists/dirb/common.txt -o "autoenum/loot/http/dirs/$dir" &;done
 					done
+				rm autoenum/loot/http/ports
 			else
 				rm autoenum/loot/http/ports
-				nikto -h $IP >> autoenum/loot/http/nikto_output
-				dirb http://$IP >> autoenum/loot/http/dirs
+				nikto -h $IP >> autoenum/loot/http/nikto_output &
+				dirb http://$IP >> autoenum/loot/http/dirs &
 		fi
 		rm autoenum/aggr_scan/raw/http_found
 	else
@@ -72,11 +87,11 @@ cat autoenum/aggr_scan/ports_and_services/services_running | sort -u | grep "smb
 if [ -s 'autoenum/aggr_scan/raw/smb_found' ]
 	then
 		mkdir -p autoenum/loot/smb
-		nmap --script=smb-check-vulns.nse --script-args=unsafe=1 -p 139,445 $IP | tee -a autoenum/loot/smb/general_vulns
-		nmap --script=smb-enum-shares.nse --script-args=unsafe=1 -p 139,445 $IP | tee -a autoenum/loot/smb/enum_shares
-		nmap --script=smb-enum-users.nse --script-args=unsafe=1 -p 139,445 $IP | tee -a autoenum/loot/smb/enum_users
-		nmap --script=smb-vuln-ms17-010.nse --script-args=unsafe=1 -p 139,445 $IP | tee -a autoenum/loot/smb/eternalblue
-		nmap --script smb-vuln-ms08-067.nse --script-args=unsafe=1 -p 445 $IP | tee -a autoenum/loot/smb/08-067
+		nmap --script=smb-check-vulns.nse --script-args=unsafe=1 -p 139,445 $IP | tee -a autoenum/loot/smb/general_vulns &
+		nmap --script=smb-enum-shares.nse --script-args=unsafe=1 -p 139,445 $IP | tee -a autoenum/loot/smb/enum_shares &
+		nmap --script=smb-enum-users.nse --script-args=unsafe=1 -p 139,445 $IP | tee -a autoenum/loot/smb/enum_users &
+		nmap --script=smb-vuln-ms17-010.nse --script-args=unsafe=1 -p 139,445 $IP | tee -a autoenum/loot/smb/eternalblue &
+		nmap --script smb-vuln-ms08-067.nse --script-args=unsafe=1 -p 445 $IP | tee -a autoenum/loot/smb/08-067 &
 		rm autoenum/aggr_scan/raw/smb_found
 	else
 		rm autoenum/aggr_scan/raw/smb_found
@@ -85,3 +100,5 @@ fi
 # IDEAS
 # implement getopts to allow for multiple nmap profiles
 # add some echo's before service enums
+# replace / w/ ' ' in services
+# add way to enum OS, pull from OS_detection, last line after OS:
