a1704e1b6546e31a2f7925c3bb44f44701c86a81
root <root@localhost.localdomain>
2020-02-02 09:14:45 -0500

    autoenum.sh

--- autoenum.sh
+++ autoenum.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+
+# IDEAS
+# implement getopts to allow for multiple nmap profiles
+# add way to enum OS, pull from OS_detection, last line after OS:
+
 IP=$1
 
 nmap_aggr="nmap -A -T4 -p- -Pn -v $IP"
@@ -28,14 +33,12 @@ if [[ ! -d "autoenum" ]];then mkdir autoenum; fi
 if [[ ! -d "autoenum/aggr_scan/raw" ]];then mkdir -p autoenum/aggr_scan/raw; fi
 if [[ ! -d "autoenum/aggr_scan/ports_and_services" ]];then  mkdir -p autoenum/aggr_scan/ports_and_services; fi
 if [[ ! -d "autoenum/loot/exploits" ]];then mkdir -p autoenum/loot/exploits; fi
-#$nmap_aggr | tee -a autoenum/aggr_scan/raw/first_pass & $nmap_aggr -oX autoenum/aggr_scan/raw/nmap_out.xml
+
 nmap -sV $IP -oX autoenum/aggr_scan/raw/nmap_out.xml & $nmap_aggr -oN autoenum/aggr_scan/raw/first_pass;
-cat autoenum/aggr_scan/raw/first_pass | grep -i "discovered" >> autoenum/aggr_scan/ports_and_services/ports_discovered
 cat autoenum/aggr_scan/raw/first_pass | grep "open" | awk -F 'Discovered' '{print $1}' | sed '/^$/d' | sed '/|/,+1 d' >> autoenum/aggr_scan/ports_and_services/services_running
 cat autoenum/aggr_scan/raw/first_pass | grep 'OS' | sed '1d' | sed '$d' | cut -d '|' -f 1 | sed '/^$/d' >> autoenum/aggr_scan/ports_and_services/OS_detection
 cat autoenum/aggr_scan/raw/first_pass | grep "script results" > autoenum/aggr_scan/ports_and_services/script_output; cat autoenum/aggr_scan/raw/first_pass | grep "|" | sed '$d' >>  autoenum/aggr_scan/ports_and_services/script_output
 
-#$nmap_aggr -oX autoenum/aggr_scan/raw/nmap_out.xml -> only needs services
 searchsploit -v --nmap -w autoenum/aggr_scan/raw/nmap_out.xml | tee -a autoenum/loot/exploits/searchsploit_nmap_first_pass
 
 
@@ -56,15 +59,12 @@ for service in $(cat autoenum/loot/services);do
 done
 rm autoenum/loot/services
 
-find autoenum/ -type d -empty -delete
-
-cat autoenum/aggr_scan/ports_and_services/services_running | sort -u | grep "http" | egrep "80|8080|443|12443|81|82|8081|8082" >> autoenum/aggr_scan/raw/http_found
+cat autoenum/aggr_scan/ports_and_services/services_running | sort -u | grep "http" | egrep "8080|443|12443|81|82|8081|8082" >> autoenum/aggr_scan/raw/http_found
 if [ ! -f 'autoenum/aggr_scan/raw/http_found' ]; then cat autoenum/aggr_scan/ports_and_services/services_running | sort -u | grep "http" >> autoenum/aggr_scan/raw/http_found; fi
 if [ -s 'autoenum/aggr_scan/raw/http_found' ]
 	then
 		mkdir -p autoenum/loot/http
 		mkdir -p autoenum/loot/http/dirs
-		cat autoenum/aggr_scan/raw/http_found | cut -d '/' -f 1 >> autoenum/loot/http/ports
 		if [ -s 'autoenum/loot/http/ports' ]
 			then
 				for port in $(cat autoenum/loot/http/ports)
@@ -72,7 +72,7 @@ if [ -s 'autoenum/aggr_scan/raw/http_found' ]
 						echo "running nikto on port $port"
 						nikto -h $IP:$port >> autoenum/loot/http/nikto_$port &
 						echo "bruteforcing dirs on $IP:$port"
-						gobuster dir -re -t 25 -u $IP:$port -w /usr/share/wordlists/dirb/common.txt -o autoenum/loot/http/dirs/raw
+						gobuster dir -re -t 25 -u $IP:$port -w /usr/share/wordlists/dirb/common.txt -o autoenum/loot/http/dirs/dirs_found
 #						echo "recursively bruteforcing..."
 #						for dir in $(cat autoenum/loot/http/dirs/raw | cut -d '(' -f 1); do gobuster dir -re -t 25 -u $dir -w /usr/share/wordlists/dirb/common.txt -o "autoenum/loot/http/dirs/$dir";done
 					done
@@ -80,7 +80,7 @@ if [ -s 'autoenum/aggr_scan/raw/http_found' ]
 			else
 				rm autoenum/loot/http/ports
 				nikto -h $IP >> autoenum/loot/http/nikto_output &
-				gobuster dir -re -t 25 -u $IP -w /usr/share/wordlists/dirb/common.txt -o autoenum/loot/http/dirs/raw
+				gobuster dir -re -t 25 -u $IP -w /usr/share/wordlists/dirb/common.txt -o autoenum/loot/http/dirs/dirs_found
 #				for dir in $(cat autoenum/loot/http/dirs/raw | cut -d '(' -f 1); do gobuster dir -re -t 25 -u $dir -w /usr/share/wordlists/dirb/common.txt -o "autoenum/loot/http/dirs/$dir";done
 		fi
 		rm autoenum/aggr_scan/raw/http_found
@@ -102,6 +102,6 @@ if [ -s 'autoenum/aggr_scan/raw/smb_found' ]
 		rm autoenum/aggr_scan/raw/smb_found
 fi
 
-# IDEAS
-# implement getopts to allow for multiple nmap profiles
-# add way to enum OS, pull from OS_detection, last line after OS:
+
+find autoenum/ -type d -empty -delete
+find autoenum/ -type f -empty -delete
