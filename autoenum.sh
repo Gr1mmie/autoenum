007aecc0ac2d64372a1062174ff0fbd35ffc8f1a
root <root@localhost.localdomain>
2020-02-02 08:49:41 -0500

    autoenum.sh

--- autoenum.sh
+++ autoenum.sh
@@ -28,13 +28,14 @@ if [[ ! -d "autoenum" ]];then mkdir autoenum; fi
 if [[ ! -d "autoenum/aggr_scan/raw" ]];then mkdir -p autoenum/aggr_scan/raw; fi
 if [[ ! -d "autoenum/aggr_scan/ports_and_services" ]];then  mkdir -p autoenum/aggr_scan/ports_and_services; fi
 if [[ ! -d "autoenum/loot/exploits" ]];then mkdir -p autoenum/loot/exploits; fi
-$nmap_aggr | tee -a autoenum/aggr_scan/raw/first_pass
+#$nmap_aggr | tee -a autoenum/aggr_scan/raw/first_pass & $nmap_aggr -oX autoenum/aggr_scan/raw/nmap_out.xml
+nmap -sV $IP -oX autoenum/aggr_scan/raw/nmap_out.xml & $nmap_aggr -oN autoenum/aggr_scan/raw/first_pass;
 cat autoenum/aggr_scan/raw/first_pass | grep -i "discovered" >> autoenum/aggr_scan/ports_and_services/ports_discovered
 cat autoenum/aggr_scan/raw/first_pass | grep "open" | awk -F 'Discovered' '{print $1}' | sed '/^$/d' | sed '/|/,+1 d' >> autoenum/aggr_scan/ports_and_services/services_running
 cat autoenum/aggr_scan/raw/first_pass | grep 'OS' | sed '1d' | sed '$d' | cut -d '|' -f 1 | sed '/^$/d' >> autoenum/aggr_scan/ports_and_services/OS_detection
 cat autoenum/aggr_scan/raw/first_pass | grep "script results" > autoenum/aggr_scan/ports_and_services/script_output; cat autoenum/aggr_scan/raw/first_pass | grep "|" | sed '$d' >>  autoenum/aggr_scan/ports_and_services/script_output
 
-$nmap_aggr -oX autoenum/aggr_scan/raw/nmap_out.xml
+#$nmap_aggr -oX autoenum/aggr_scan/raw/nmap_out.xml -> only needs services
 searchsploit -v --nmap -w autoenum/aggr_scan/raw/nmap_out.xml | tee -a autoenum/loot/exploits/searchsploit_nmap_first_pass
 
 
@@ -57,7 +58,8 @@ rm autoenum/loot/services
 
 find autoenum/ -type d -empty -delete
 
-cat autoenum/aggr_scan/ports_and_services/services_running | sort -u | grep "http" | egrep "8080|443|12443|81|82|8081|8082" >> autoenum/aggr_scan/raw/http_found
+cat autoenum/aggr_scan/ports_and_services/services_running | sort -u | grep "http" | egrep "80|8080|443|12443|81|82|8081|8082" >> autoenum/aggr_scan/raw/http_found
+if [ ! -f 'autoenum/aggr_scan/raw/http_found' ]; then cat autoenum/aggr_scan/ports_and_services/services_running | sort -u | grep "http" >> autoenum/aggr_scan/raw/http_found; fi
 if [ -s 'autoenum/aggr_scan/raw/http_found' ]
 	then
 		mkdir -p autoenum/loot/http
@@ -68,7 +70,7 @@ if [ -s 'autoenum/aggr_scan/raw/http_found' ]
 				for port in $(cat autoenum/loot/http/ports)
 					do
 						echo "running nikto on port $port"
-						nikto -h $IP:$port >> autoenum/loot/http/nikto_$port
+						nikto -h $IP:$port >> autoenum/loot/http/nikto_$port &
 						echo "bruteforcing dirs on $IP:$port"
 						gobuster dir -re -t 25 -u $IP:$port -w /usr/share/wordlists/dirb/common.txt -o autoenum/loot/http/dirs/raw
 #						echo "recursively bruteforcing..."
@@ -77,7 +79,7 @@ if [ -s 'autoenum/aggr_scan/raw/http_found' ]
 				rm autoenum/loot/http/ports
 			else
 				rm autoenum/loot/http/ports
-				nikto -h $IP >> autoenum/loot/http/nikto_output
+				nikto -h $IP >> autoenum/loot/http/nikto_output &
 				gobuster dir -re -t 25 -u $IP -w /usr/share/wordlists/dirb/common.txt -o autoenum/loot/http/dirs/raw
 #				for dir in $(cat autoenum/loot/http/dirs/raw | cut -d '(' -f 1); do gobuster dir -re -t 25 -u $dir -w /usr/share/wordlists/dirb/common.txt -o "autoenum/loot/http/dirs/$dir";done
 		fi
@@ -90,11 +92,11 @@ cat autoenum/aggr_scan/ports_and_services/services_running | sort -u | grep "smb
 if [ -s 'autoenum/aggr_scan/raw/smb_found' ]
 	then
 		mkdir -p autoenum/loot/smb
-		nmap --script=smb-check-vulns.nse --script-args=unsafe=1 -p 139,445 $IP | tee -a autoenum/loot/smb/general_vulns &
-		nmap --script=smb-enum-shares.nse --script-args=unsafe=1 -p 139,445 $IP | tee -a autoenum/loot/smb/enum_shares &
-		nmap --script=smb-enum-users.nse --script-args=unsafe=1 -p 139,445 $IP | tee -a autoenum/loot/smb/enum_users &
-		nmap --script=smb-vuln-ms17-010.nse --script-args=unsafe=1 -p 139,445 $IP | tee -a autoenum/loot/smb/eternalblue &
-		nmap --script smb-vuln-ms08-067.nse --script-args=unsafe=1 -p 445 $IP | tee -a autoenum/loot/smb/08-067 &
+		nmap --script=smb-check-vulns.nse --script-args=unsafe=1 -p 139,445 $IP -oN autoenum/loot/smb/general_vulns;sleep 3
+		nmap --script=smb-enum-shares.nse --script-args=unsafe=1 -p 139,445 $IP -oN autoenum/loot/smb/enum_shares;sleep 3
+		nmap --script=smb-enum-users.nse --script-args=unsafe=1 -p 139,445 $IP -oN autoenum/loot/smb/enum_users;sleep 3
+		nmap --script=smb-vuln-ms17-010.nse --script-args=unsafe=1 -p 139,445 $IP -oN autoenum/loot/smb/eternalblue;sleep 3
+		nmap --script smb-vuln-ms08-067.nse --script-args=unsafe=1 -p 445 $IP -oN autoenum/loot/smb/08-067
 		rm autoenum/aggr_scan/raw/smb_found
 	else
 		rm autoenum/aggr_scan/raw/smb_found
